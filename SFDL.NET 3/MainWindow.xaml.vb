3bc19d864987a1e4d7165249c081736e72d12712
n0ix <n0ix@protonmail.com>
2016-03-24 12:04:28 +0100

    Added Settings Window

--- SFDL.NET 3/MainWindow.xaml.vb
+++ SFDL.NET 3/MainWindow.xaml.vb
@@ -1,3 +1,7 @@
 ﻿Public Class MainWindow
+    Private Sub cmd_Settings_Click(sender As Object, e As RoutedEventArgs) Handles cmd_Settings.Click
 
+
+
+    End Sub
 End Class
---
+++ SFDL.NET 3/SettingsWindow.xaml.vb
@@ -0,0 +1,3 @@
+﻿Public Class SettingsWindow
+
+End Class
